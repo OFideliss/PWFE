217b34a82f39cc63fa79a154d0db45ecf8679c69
OFideliss <otavio-fidelis@outlook.com>
2023-10-06 21:49:16 -0300

    06/10/23

--- ProjetoAngular/meu-primeiro-angular/src/app/app-routing.module.ts
+++ ProjetoAngular/meu-primeiro-angular/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'contato', component: ContatoComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
