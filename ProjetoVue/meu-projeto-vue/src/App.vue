4495a516b2fc55c13adb86296fa7722f6fdb91e9
OFideliss <otavio-fidelis@outlook.com>
2023-10-10 21:53:26 -0300

    10/10/23

---
+++ ProjetoVue/meu-projeto-vue/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Meu Catálogo de Filmes</h1>
+      <br>
+      <button onclick="trocarTela()">Catálago Livros</button>
+    </header>
+    <main>
+      <div id="Filme">
+        <CatalogoDeFilmes />
+      </div>
+      <div id="Livro" style="display: none;">
+        <CatalogoDeLivros />
+      </div>
+    </main>
+    <footer>
+      <h1>Meu Footer do Catálogo de Filmes</h1>
+    </footer>
+  </div>
+</template>
+  
+<script>
+
+import CatalogoDeFilmes from "@/components/CatalogoDeFilmes.vue";
+import CatalogoDeLivros from "@/components/CatalogoDeLivros.vue";
+
+export default {
+
+  methods: {
+    trocarTela() {
+      var telaLivro = document.getElementById("Livro");
+      var telaFilme = document.getElementById("Filme");
+      if (telaLivro.style.display === "none") {
+        telaFilme.style.display = "none";
+        //this.filmes.push({ titulo: this.novoFilme, assistido: false });
+        //this.novoFilme = "";
+        telaLivro.style.display = "block";
+      }
+    },
+  },
+  components: {
+    CatalogoDeFilmes,
+    CatalogoDeLivros,
+  },
+
+
+};
+</script>
+  
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+
+header {
+
+  background-color: #35495e;
+  color: #fff;
+  padding: 10px 0;
+}
+
+footer {
+
+  background-color: #35495e;
+  color: #fff;
+  padding: 10px 0;
+
+  position: absolute;
+  bottom: 0;
+  width: 99%;
+  /* height: 2.5rem; */
+}</style>
---
+++ ProjetoVue/meu-projeto-vue/src/components/CatalogoDeLivros.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="catalogo-livros">
+        <h2>Catálogo de Livros</h2>
+        <input v-model="novoLivro" @keyup.enter="adicionarLivro" placeholder="Digite um novo livro" />
+        <button @click="adicionarLivro">Adicionar</button>
+        <ul>
+            <li v-for="(livro, index) in livros" :key="index">
+                {{ livro.titulo }}
+                <button @click="alternarStatus(livro)">Marcar como {{
+                    livro.assistido ? 'Não Assistido' : 'Assistido' }}</button>
+
+            </li>
+        </ul>
+    </div>
+</template>
+    
+<script>
+export default {
+    data() {
+        return {
+            novoLivro: "",
+            livros: [],
+        };
+    },
+    methods: {
+        adicionarLivro() {
+            if (this.novolivro.trim() !== "") {
+                this.livros.push({ titulo: this.novoLivro, assistido: false });
+                this.novolivro = "";
+            }
+        },
+        alternarStatus(livro) {
+            livro.assistido = !livro.assistido;
+        },
+    },
+};
+</script>
+    
+<style scoped>
+.catalogo-livros {
+    margin: 20px;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    margin: 10px 0;
+}
+</style>
